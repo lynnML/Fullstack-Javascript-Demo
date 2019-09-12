e1cb747105ddb130425f7201b2308bcf94cbbbda
P350376 <lynn.m.longboy@kp.org>
2019-09-12 14:34:59 -0700

    State functions and logic

--- src/Application.js
+++ src/Application.js
@@ -5,36 +5,48 @@ class Application extends Component{
     constructor(props){
         super(props);
 
+        this.state = {
+            count: 0,
+            overTen: false
+        }
 
     }
 
-    componentWillMount(pros, state){ //deprectated in 2018 - UNSAFE_componentWillMount()
+    //writing our own methods for states
 
+    handleClick = () => {  //this is just a button equal to a function
+        console.log("Clicked");
+        this.setState({count: this.state.count + 1})
     }
 
-    componentDidMount(props, state){  //Best Practice to make calls to fetch data
-        console.log("mounted with", props, state);
+    //everytime a state is changed, this function is called
+    componentDidUpdate(props, state){
+        console.log("Updated from", state, "to", this.state);
+         //doing something weird, NOT RECOMMENDED --> add this.setState({count: this.state.count + 1})  --> Error: Maximun dept exceeded
+         if(this.state.count > 5 && this.state.count != state.count && !this.state.overTen){
+             console.log("UPdating over 5")
+             this.setState({overTen: true}) //gets stuck be cause when overTen gets updated, this function gets called again
+         }
     }
 
-    componentWillReceiveProps(props){
+   
 
-    }
 
-    componentWillUpdate(props, state){ 
-
-    }
-
-    componentDidUpdate(props, state){ //Safer way to update components
-
-    }
 
     render(){ //method returns html or text or variables or etc....
-        let  name = "Lynn";
-        var test = "is this used outside of render function"
+        let {count} = this.state  //basically gets the count variable for the state
+    
         return (
             <div>
-                <h1>Hello World</h1>
-                <span>This is {name}</span> 
+                <h1>You clicked the button {count}</h1>
+                {(this.state.overTen) ? 
+                    <h3>Beat the highest score of 5!</h3>
+                    : null
+                }
+              
+                    <button onClick ={(e) => this.handleClick()}>Click Me</button>
+                    {/* <button onClick ={this.handleClick()}>Click Me</button>  will not work, when it mounts, it calls function once, button does not work after*/}
+                    
             </div>
         );
     }
